b95d73c3a8f0b573a752c1b0294b361a3ebeb995
Ahmed Mattar <AceMattar@gmail.com>
2022-07-05 15:07:05 +0200

    feat:auth: add boilerplate for error handling middleware

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import { body } from "express-validator";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
@@ -13,7 +13,18 @@ router.post(
       .withMessage("Password must be between 4 and 20 charactrs"),
   ],
   (req: Request, res: Response) => {
+    // check if any errors exist from running the two validator middlewares
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
+    }
+
     const { email, password } = req.body;
+
+    console.log("Creating a user...");
+
+    res.send({});
   }
 );
 
