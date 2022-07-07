c901ad9370b82438a9bf684710e43e76612a8118
Ahmed Mattar <AceMattar@gmail.com>
2022-07-07 15:36:19 +0200

    feat:auth: implement password compare static method

--- auth/src/services/password.ts
+++ auth/src/services/password.ts
@@ -14,5 +14,16 @@ export class Passowrd {
     return `${buf.toString("hex")}.${salt}`;
   }
 
-  static compare(storedPassword: string, suppliedPassword: string) {}
+  /**
+   *
+   * @param storedPassword  the hashed password plus the salt added in toHash
+   * @param suppliedPassword  the password given by the user
+   * @return Boolean
+   */
+  static async compare(storedPassword: string, suppliedPassword: string) {
+    const [hashedPassword, salt] = storedPassword.split(".");
+    const buf = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
+
+    return buf.toString("hex") === hashedPassword;
+  }
 }
