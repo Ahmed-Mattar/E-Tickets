c5f472ddfc52691ba2833f88468c90c0ddbb38ad
Ahmed Mattar <AceMattar@gmail.com>
2022-07-09 14:10:13 +0200

    feat:auth: use a middleware to validate body errors

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from "express";
-import { body, validationResult } from "express-validator";
+import { body } from "express-validator";
 import jwt from "jsonwebtoken";
 import { RequestValidationError } from "../errors/request-validation-error";
+import { validateRequest } from "../middlewares/validate-request";
 import { BadRequestError } from "../errors/bad-requrest-error";
 import { User } from "../models/user";
 
@@ -16,14 +17,8 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be between 4 and 20 charactrs"),
   ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    // check if any errors exist from running the two validator middlewares
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-
     // step 1 check if the email already exist
     const { email, password } = req.body;
 
