ca46c1899d8ac0595c5de4a8b4753533335dd3a6
Ahmed Mattar <AceMattar@gmail.com>
2022-07-17 21:30:40 +0200

    tickets:tests: add test for sending invalid price

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -40,6 +40,23 @@ it("returns an error if an invalid title is provided", async () => {
     .expect(400);
 });
 
-it("returns an error if an invalid price is provided", async () => {});
+it("returns an error if an invalid price is provided", async () => {
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", signin())
+    .send({
+      title: "title",
+      price: -10,
+    })
+    .expect(400);
+
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", signin())
+    .send({
+      title: "title",
+    })
+    .expect(400);
+});
 
 it("creates a ticket with valid inputs", async () => {});
