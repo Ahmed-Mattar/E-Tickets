829bdae3d790f9a158ed9cb85e4a647231350b2d
Ahmed Mattar <AceMattar@gmail.com>
2022-07-18 04:12:16 +0200

    tickets: add check for the owner user in update endpoint

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -19,6 +19,10 @@ router.put(
       throw new NotFoundError();
     }
 
+    if (ticket.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
     res.send(ticket);
   }
 );
