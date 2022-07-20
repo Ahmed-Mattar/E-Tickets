29caa754b14b1ee57b0043eb1f56ed69e5943be4
Ahmed Mattar <AceMattar@gmail.com>
2022-07-20 19:28:07 +0200

    orders:routes: validate incoming request

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,16 @@
 import express, { Request, Response } from "express";
+import { requireAuth, validateRequest } from "@e-tickets/common";
+import { body } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/orders", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  "/api/orders",
+  requireAuth,
+  [body("ticketId").not().isEmpty().withMessage("TicketId must be provided")],
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as newOrderRouter };
