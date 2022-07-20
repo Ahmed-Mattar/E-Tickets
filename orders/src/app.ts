be45b412ad45f735e1aec6051712afda15d3303d
Ahmed Mattar <AceMattar@gmail.com>
2022-07-20 19:21:51 +0200

    orders: scaffold routes

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -2,11 +2,11 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
-import { createTicketRouter } from "./routes/new";
-import { showTicketRouter } from "./routes/show";
-import { indexTicketRouter } from "./routes/index";
 import { errorHandler, NotFoundError, currentUser } from "@e-tickets/common";
-import { updateTicketRouter } from "./routes/update";
+import { deleteOrderRouter } from "./routes/delete";
+import { indexOrderRouter } from "./routes";
+import { newOrderRouter } from "./routes/new";
+import { showOrderRouter } from "./routes/show";
 
 const app = express();
 //to make express aware that it's behind a proxy ingress
@@ -20,10 +20,10 @@ app.use(
   })
 );
 app.use(currentUser);
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
+app.use(deleteOrderRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
+app.use(indexOrderRouter);
 app.all("*", async (req, res) => {
   throw new NotFoundError();
 });
