18bd8abd123d07ba3c95f0d85355ede0d953adb7
Ahmed Mattar <AceMattar@gmail.com>
2022-07-26 02:22:42 +0200

    tickets:test: add test for optimistic concurrency control

---
+++ tickets/src/models/__test__/ticket.test.ts
@@ -0,0 +1,30 @@
+import { Ticket } from "../ticket";
+
+it("optimistic concurrency control", async () => {
+  // create an instance of a ticket
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 10,
+    userId: "123",
+  });
+  // save the ticket to the database
+  await ticket.save();
+  // fetch the ticket twice
+  const firstInstance = await Ticket.findById(ticket.id); //version 0
+  const secondInstance = await Ticket.findById(ticket.id); //version 0
+  // make changes to the tickets we fetched
+  firstInstance!.set({ price: 20 });
+  secondInstance!.set({ price: 15 });
+
+  // save the first fetched ticket
+  await firstInstance!.save(); // it's now version 1
+  // save the second fetched ticket  and expect an error
+
+  try {
+    await secondInstance!.save();
+  } catch (error) {
+    return;
+  }
+
+  throw new Error("Shouldn't reach this point");
+});
