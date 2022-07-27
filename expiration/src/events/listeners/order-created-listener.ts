c25cabd84aab449620d16d8c6db6dee623f53ff7
Ahmed Mattar <AceMattar@gmail.com>
2022-07-28 00:51:54 +0200

    expiration:nats: add order created listener

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,15 @@
+import {
+  listener,
+  OrderCreatedEvent,
+  OrderStatus,
+  Subjects,
+} from "@e-tickets/common";
+import { Message } from "node-nats-streaming";
+import { queueGroupName } from "./queue-group-name";
+export class OrderCreatedListener extends listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated;
+
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
+}
