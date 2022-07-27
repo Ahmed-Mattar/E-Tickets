d27a9dd0fc9ebbf7f6bed9c97922debd3a833f46
Ahmed Mattar <AceMattar@gmail.com>
2022-07-28 01:20:34 +0200

    expiration: calculate delay and pass it to bull queue

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -6,10 +6,25 @@ import {
 } from "@e-tickets/common";
 import { Message } from "node-nats-streaming";
 import { queueGroupName } from "./queue-group-name";
+import { expirationQueue } from "../../queues/expiration-queue";
+
 export class OrderCreatedListener extends listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
 
   queueGroupName = queueGroupName;
 
-  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+
+    await expirationQueue.add(
+      {
+        orderId: data.id,
+      },
+      {
+        delay: delay,
+      }
+    );
+
+    msg.ack();
+  }
 }
--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,4 +1,5 @@
 import { natsWrapper } from "./nats-wrapper";
+import { OrderCreatedListener } from "./events/listeners/order-created-listener";
 
 const start = async () => {
   if (!process.env.NATS_CLIENT_ID) {
@@ -26,6 +27,8 @@ const start = async () => {
     // windows platform issues on SIGINT and SIGTERM
     process.on("SIGINT", () => natsWrapper.client.close());
     process.on("SIGTERM", () => natsWrapper.client.close());
+
+    new OrderCreatedListener(natsWrapper.client).listen();
   } catch (error) {
     console.log(error);
   }
