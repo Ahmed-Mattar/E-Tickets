1eeb1cd7215a5941c6de5c514f3d57581830959c
Ahmed Mattar <AceMattar@gmail.com>
2022-07-27 22:31:25 +0200

    tickets:nats: wire nats to lsiten for order created/cancelled

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 import { app } from "./app";
 import { natsWrapper } from "./nats-wrapper";
-
+import { OrderCreatedListener } from "./events/listeners/order-created-listener";
+import { OrderCancelledListener } from "./events/listeners/order-cancelled-listener";
 const start = async () => {
   // set using kubectl secrets
   if (!process.env.JWT_KEY) {
@@ -37,6 +38,10 @@ const start = async () => {
     // windows platform issues on SIGINT and SIGTERM
     process.on("SIGINT", () => natsWrapper.client.close());
     process.on("SIGTERM", () => natsWrapper.client.close());
+
+    new OrderCreatedListener(natsWrapper.client).listen();
+    new OrderCancelledListener(natsWrapper.client).listen();
+
     // instead of localhost url use the service url followed by the port that mongoose use
     await mongoose.connect(process.env.MONGO_URI);
     console.log("connected to mongoDB");
