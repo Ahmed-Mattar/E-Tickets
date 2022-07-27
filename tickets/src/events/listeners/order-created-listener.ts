130f69116f79eeb7d034a8594cb9a072538012ce
Ahmed Mattar <AceMattar@gmail.com>
2022-07-27 21:43:09 +0200

    tickets:nats: add onMessage implementation for orderCreatedEvent

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -1,11 +1,25 @@
 import { listener, OrderCreatedEvent, Subjects } from "@e-tickets/common";
 import { Message } from "node-nats-streaming";
 import { queueGroupName } from "./queue-group-name";
+import { Ticket } from "../../models/ticket";
 
 export class OrderCreatedListener extends listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
 
   queueGroupName = queueGroupName;
 
-  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    // Find the ticket that the order is reserving
+    const ticket = await Ticket.findById(data.ticket.id);
+    // if no ticket, throw error
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+    // Mark the ticket as being reserved by setting it's orderId property
+    ticket.set({ orderId: data.id });
+    // save the ticket
+    await ticket.save();
+    // ack the message
+    msg.ack();
+  }
 }
