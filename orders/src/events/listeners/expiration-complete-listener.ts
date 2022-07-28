d42f52ce35d0925fef01f6d1f963982bcaad4819
Ahmed Mattar <AceMattar@gmail.com>
2022-07-28 03:49:03 +0200

    orders:nats: publish order cancelled event

--- orders/src/events/listeners/expiration-complete-listener.ts
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -7,6 +7,7 @@ import {
 } from "@e-tickets/common";
 import { queueGroupName } from "./queue-group-name";
 import { Order } from "../../models/order";
+import { OrderCancelledPublisher } from "../publishers/order-cancelled-publisher";
 
 export class ExpirationCompleteListener extends listener<ExpirationCompleteEvent> {
   readonly subject = Subjects.ExpirationComplete;
@@ -14,7 +15,7 @@ export class ExpirationCompleteListener extends listener<ExpirationCompleteEvent
   queueGroupName = queueGroupName;
 
   async onMessage(data: ExpirationCompleteEvent["data"], msg: Message) {
-    const order = await Order.findById(data.orderId);
+    const order = await Order.findById(data.orderId).populate("ticket");
 
     if (!order) {
       throw new Error("Order not found");
@@ -22,6 +23,16 @@ export class ExpirationCompleteListener extends listener<ExpirationCompleteEvent
 
     order.set({
       status: OrderStatus.Cancelled,
+    });
+
+    await order.save();
+
+    await new OrderCancelledPublisher(this.client).publish({
+      id: order.id,
+      version: order.version,
+      ticket: {
+        id: order.ticket.id,
+      },
     });
 
     msg.ack();
