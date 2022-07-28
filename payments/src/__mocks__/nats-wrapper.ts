9c6f71a1fdba534002e26c571d8a6b96c418e861
Ahmed Mattar <AceMattar@gmail.com>
2022-07-28 16:41:32 +0200

    payments: add boilerplate for payments

---
+++ payments/src/__mocks__/nats-wrapper.ts
@@ -0,0 +1,13 @@
+// fake implementation
+// jest function to test the link between the code
+export const natsWrapper = {
+  client: {
+    publish: jest
+      .fn()
+      .mockImplementation(
+        (subject: string, data: string, callback: () => void) => {
+          callback();
+        }
+      ),
+  },
+};
---
+++ payments/src/test/setup.ts
@@ -0,0 +1,49 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+
+jest.mock("../nats-wrapper");
+// connect to the in memory mongo server before any test
+let mongo: any;
+beforeAll(async () => {
+  process.env.JWT_KEY = "asdasda";
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri, {});
+});
+
+// to reset data before each test we run
+beforeEach(async () => {
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+// stop server after all the tests finished
+afterAll(async () => {
+  await mongo.stop();
+});
+
+function signin() {
+  // Build a JWT payload. {id,email}
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: "test@test.com",
+  };
+  //create the JWT!
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+  //build session object {jwt: MY_JWT}
+  const session = { jwt: token };
+  // Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+  //TAKE JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+  // return a string thats the cookie with the encoded data
+  return [`session=${base64}`];
+}
+
+export { signin };
